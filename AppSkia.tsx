b93a04fd65ef2d5c055629580dcb8ad87a1a53ac
Gabriel Alvarez <galvarez.chile@gmail.com>
2025-05-14 11:05:02 -0400

    mejoras skia

--- AppSkia.tsx
+++ AppSkia.tsx
@@ -1,22 +1,31 @@
 import React, {useRef, useState} from 'react';
-import {View, StyleSheet, PanResponder} from 'react-native';
+import {View, StyleSheet, PanResponder, Dimensions} from 'react-native';
+import ViewShot from 'react-native-view-shot';
 import {
   Canvas,
   Path,
   Skia,
   SkPath,
   PaintStyle,
+  Image,
+  useImage,
 } from '@shopify/react-native-skia';
 
 const AppSkia = () => {
+  const svgRef = useRef(null);
+
   const [paths, setPaths] = useState<SkPath[]>([]);
+
   const currentPath = useRef<SkPath | null>(null);
+
   const [, forceUpdate] = useState(0); // Para forzar render
+
+  const background = useImage(require('./assets/bg_notebook.jpg'));
 
   const paint = Skia.Paint();
   paint.setStyle(PaintStyle.Stroke);
-  paint.setStrokeWidth(2);
-  paint.setColor(Skia.Color('white'));
+  paint.setStrokeWidth(1.5);
+  paint.setColor(Skia.Color('black'));
   paint.setAntiAlias(true);
 
   const panResponder = useRef(
@@ -49,11 +58,28 @@ const AppSkia = () => {
 
   return (
     <View style={styles.container} {...panResponder.panHandlers}>
-      <Canvas style={styles.canvas}>
-        {paths.map((path, index) => (
-          <Path key={index} path={path} paint={paint} />
-        ))}
-      </Canvas>
+      <ViewShot
+        ref={svgRef}
+        style={styles.viewShot}
+        options={{format: 'jpg', quality: 0.9}}>
+        <Canvas style={styles.canvas}>
+          {background && (
+            <Image
+              image={background}
+              x={0}
+              y={0}
+              antiAlias
+              opacity={0.3}
+              fit={'fill'}
+              width={Dimensions.get('screen').width * 1.0}
+              height={Dimensions.get('screen').height * 0.95}>
+              {paths.map((path, index) => (
+                <Path key={index} path={path} paint={paint} />
+              ))}
+            </Image>
+          )}
+        </Canvas>
+      </ViewShot>
     </View>
   );
 };
@@ -61,10 +87,15 @@ const AppSkia = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'transparent',
+  },
+  viewShot: {
+    flex: 1,
+    backgroundColor: 'transparent',
   },
   canvas: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: 'transparent',
   },
 });
 
